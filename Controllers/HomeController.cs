9d391f33475d979b75ec37f0859c40cecf811ec6
dns-lector <157026463+dns-lector@users.noreply.github.com>
2026-08-15 18:08:14 +0300

    Models

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using ASP_P42.Models;
+using ASP_P42.Models.Home.Models;
 using ASP_P42.Services.Hash;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,37 @@ namespace ASP_P42.Controllers
             ViewBag.Hash = _hashService.GetHashCode();
             ViewData["digest"] = digest;
             return View();
+        }
+
+        public IActionResult Models(String? id)  // id - опціональний параметр маршруту
+        {
+            // Одна з центральних задач контролерів - підготовка і 
+            // трансформація моделей
+            HomeModelsViewModel viewModel = new()
+            {
+                PageTitle = "Моделі в ASP",
+                Intro = "Модель (у MVC) - архітектурна частина проєкту, яка відповідає за \r\n    взаємодію з даними.\r\n    Модель (в ASP) - клас (об'єкт), призначений для передачі даних \r\n    (DTO - Data Transfer Object, Entity)",
+                ClassificationHeader = "Розрізняють декілька типів моделей за призначенням:",
+                ClassificationList = [
+                    "Модель представлення (ViewModel або PageModel) - дані, з яких будується сторінка (або її частина - представлення)",
+                    "Модель форми (FormModel) - дані, що заповнюються користувачем на сторінці і передаються на обробку.",
+                    "Модель даних (DTO - Data Transfer Object, Entity) - дані, що  зберігаються на постійній основі, частіше за все у БД. ",
+                ],
+                ExampleHeader = "Наприклад, для моделі \"користувач\":",
+                ExampleList = [
+                    "Модель форми (реєстрація) - логін, пароль, повтор пароля, ...",
+                    "Модель даних (у БД) - логін, DK(хеш паролю), сіль, ..., дата створення",
+                    "Модель представлення (профіль або кабінет) - логін, ..., дата створення \r\n        (паролів немає)",
+                ],
+            };
+
+            return id == "json" ? Json(viewModel) : View(viewModel);   // передаємо модель (об'єкт) до представлення
+        }
+
+        [HttpPost]  // Обмежуємо використання сторінки лише методом POST
+        public IActionResult ModelsForm(HomeModelsFormModel formModel)
+        {
+            return View(formModel);
         }
 
         public IActionResult Razor()
@@ -48,3 +80,9 @@ namespace ASP_P42.Controllers
         }
     }
 }
+/* Д.З. Розширити форму на сторінці /Home/Models
+ * додати поля з різними типами: "галочка", радіокнопки,
+ * вибір дати, кольору, числа тощо
+ * Адаптувати модель ASP для прийому відповідних даних.
+ * Додати скріншоти.
+ */
---
+++ Models/Home/Models/HomeModelsFormModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_P42.Models.Home.Models
+{
+    public class HomeModelsFormModel
+    {
+        [FromForm(Name = "user-login")]
+        public String UserLogin { get; set; } = null!;
+
+        [FromForm(Name = "user-password")]
+        public String UserPassword { get; set; } = null!;
+    }
+}
