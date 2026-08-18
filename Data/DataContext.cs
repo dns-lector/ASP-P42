c3a04c13745633b29b35b1b0d461336942201115
dns-lector <157026463+dns-lector@users.noreply.github.com>
2026-08-18 18:29:27 +0300

    Saves

---
+++ Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿namespace ASP_P42.Data
+{
+    public class DataContext
+    {
+    }
+}
+
+/*
+Entity Framework - інструмент спрощеної роботи з БД
+Надає засоби для уніфікації - для роботи з БД вживаються
+команди основної мови проєкту (C#), однакові (або еквівалентні)
+для різних СУБД.
+
+Для підключення EF додаємо пакети NuGet:
+- загальні інтерфейси -- Microsoft.EntityFrameworkCore
+- їх імплементація під конкретну БД -- Microsoft.EntityFrameworkCore.SqlServer
+- інструментарій командного рядка (зокрема, міграції) -- Microsoft.EntityFrameworkCore.Tools
+ 
+Створюємо директорію (шар) проєкту Data
+Entities - відбивають структуру таблиць БД. Для частини "Users":
+
+[UsersData]        [UserAccesses]     [UserRoles]
+[Id]      ---\     [Id]          /--- [Id]
+[FullName]    \----[UsedId]     /     [Name]
+[Email]            [RoleId] ---/      [CreateLevel]
+[Phone]            [Login]            [ReadLevel]
+[Birthdate]        [Salt]             [UpdateLevel]
+[RegisteredAt]     [Dk]               [DeleteLevel]
+[DeletedAt]
+ */
