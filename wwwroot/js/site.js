fef4928986f5f83fa90865f2355d51e679538ea1
dns-lector <157026463+dns-lector@users.noreply.github.com>
2026-08-22 16:34:44 +0300

    Auth Modal

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener('submit', e => {
+    const form = e.target;
+    if (form.id == 'auth-form') {
+        // зупиняємо автоматичне надсилання форми
+        e.preventDefault();
+        // вилучаємо дані, що передаються
+        const formData = new FormData(form);
+        const login = formData.get("auth-login");
+        const password = formData.get("auth-password");
+        // здійснюємо попередню перевірку (на порожність)
+        let errorMessage = "";
+        if (login.trim().length === 0) {
+            errorMessage += "Логін не може бути порожнім.\n";
+        }
+        if (password.trim().length === 0) {
+            errorMessage += "Пароль не може бути порожнім.\n";
+        }
+        const err = document.getElementById("auth-modal-error");
+        if (errorMessage.length > 0) {
+            err.innerText = errorMessage;
+            err.style.visibility = "visible";
+        }
+        else {
+            err.innerText = "";
+            err.style.visibility = "hidden";
+        }
+        console.log(login, password);
+    }
+});
