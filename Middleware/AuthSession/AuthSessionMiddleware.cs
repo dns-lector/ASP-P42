73b5f3db767e039e4fc31101768e1482def8b83e
dns-lector <157026463+dns-lector@users.noreply.github.com>
2026-08-27 18:36:22 +0300

    Saves

--- Middleware/AuthSession/AuthSessionMiddleware.cs
+++ Middleware/AuthSession/AuthSessionMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using ASP_P42.Data;
+using ASP_P42.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASP_P42.Middleware.AuthSession
 {
@@ -10,13 +12,35 @@ namespace ASP_P42.Middleware.AuthSession
     {
         private readonly RequestDelegate _next = next;
 
-        public async Task InvokeAsync(HttpContext context)
+        public async Task InvokeAsync(
+            HttpContext context,       // інжекція через метод
+            DataContext dataContext    // порядок ролі не грає, тільки тип
+        )
         {
             // context, що передається параметром, це той самий 
             // HttpContext, що доступний з контролерів
             // Відповідно, до нього можна закласти дані, що можуть
             // бути використані як у контролерах, так і у представленнях
             context.Items.Add("itemKey", "Item Value");
+
+            // перевіряємо, чи є у сесії елемент з ключем "userAccessId"
+            String authKey = "userAccessId";
+            if (context.Session.Keys.Contains(authKey))
+            {
+                String userAccessId = context.Session.GetString(authKey)!;
+                // це має бути валідний рядок з БД - перевіряємо...
+                UserAccess? userAccess = dataContext
+                    .UserAccesses
+                    .Include(ua => ua.UserData)  // інструкція для заповнення
+                    .Include(ua => ua.UserRole)  // навігаційних властивостей
+                    .AsNoTracking()              // Вимкнення стеження змін
+                    .FirstOrDefault(ua => ua.Id.ToString() == userAccessId);
+                if (userAccess != null)
+                {
+                    // знайдено підтвердження допуску, передаємо до контексту
+                    context.Items.Add(authKey, userAccess);
+                }
+            }
 
             // передача роботи до наступного Middleware
             await _next(context);
